c096e1cb92f234e1722bb7e13f7540bd679b0900
Steven Kelsey <steven.kelsey@revature.com>
2017-02-21 17:06:37 -0500

    src and pom.xml added

---
+++ src/main/java/com/steveace6/LwtApiApplication.java
@@ -0,0 +1,12 @@
+package com.steveace6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LwtApiApplication.class, args);
+	}
+}
